4a8d1e0d90da49b2240143696d9e216f467a0e25
Daquer <leandro.moniz@gmail.com>
2016-10-19 01:29:19 -0200

    Avanços na obtenção de dados de course do Facebook pelo Job

--- src/br/com/fitrank/job/JobExtracao.java
+++ src/br/com/fitrank/job/JobExtracao.java
@@ -1,17 +1,15 @@
 package br.com.fitrank.job;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.impl.jdbcjobstore.Constants;
 
+import br.com.fitrank.modelo.Course;
 import br.com.fitrank.modelo.Pessoa;
+import br.com.fitrank.service.CourseServico;
 import br.com.fitrank.service.PessoaServico;
 import br.com.fitrank.service.PostFitnessServico;
 import br.com.fitrank.util.ConstantesFitRank;
@@ -19,8 +17,10 @@ import br.com.fitrank.util.Logger;
 
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
-import com.restfb.Parameter;
 import com.restfb.FacebookClient.AccessToken;
+import com.restfb.Parameter;
+import com.restfb.json.JsonArray;
+import com.restfb.json.JsonException;
 import com.restfb.json.JsonObject;
 
 public class JobExtracao implements Job {
@@ -31,8 +31,11 @@ public class JobExtracao implements Job {
 //		InputStream input = null;
 //	    Properties prop = new Properties();
 	    PostFitnessServico postFitnessServico = new PostFitnessServico();
+	    CourseServico courseServico = new CourseServico();
 	    PessoaServico pessoaServico = new PessoaServico();
 	    List<Pessoa> pessoas = new ArrayList<Pessoa>();
+	    List<Course> coursesPessoa = new ArrayList<Course>();
+	    List<String> coursesRequest = new ArrayList<String>();
 	    
 //	    input = getClass().getClassLoader().getResourceAsStream("config.properties");
 //	   
@@ -60,7 +63,75 @@ public class JobExtracao implements Job {
 	    	
 	    		pessoaServico.atualizaPessoaServico(pessoa, false);
 	    	}
+	    	
+	    	coursesPessoa = courseServico.leCoursePorIdPessoa(pessoa.getId_usuario());
+	    	
+	    	String coursesStr = "";
+	    	int courseLimit = 50;
+	    	
+	    	Logger.insertLog("size--> " + coursesPessoa.size());
+	    	for ( int i = 0; i < coursesPessoa.size(); i++) {
+	    		Course course = coursesPessoa.get(i);
+	    		
+	    		
+	    		if (i % 50 == 0){
+	    			coursesStr = course.getId_course();
+	    		} else {
+	    			coursesStr += "," + course.getId_course();
+	    		} 
+//	    		coursesRequest.add(course.getId_course());
+	    		Logger.insertLog("i--> " + i + " id--> " + course.getId_course());
+	    	
+	    		if (i == courseLimit - 1 || i == coursesPessoa.size() - 1){
+			    	JsonObject jsonCourses = facebookClient.fetchObject("fitness.course", JsonObject.class, Parameter.with("ids", coursesStr));
+			    	
+//			    	ArrayList<JsonObject> jsonCoursesArray = jsonCourses.toJsonArray(jsonCourses.names());
+			    	JsonArray jsonCoursesArray = jsonCourses.toJsonArray(jsonCourses.names());
+			    	
+			    	for( int j = 0; j < jsonCoursesArray.length(); j++){
+			    		Logger.insertLog("j--> " + j);
+			    		JsonObject jsonCourse = (JsonObject) jsonCoursesArray.get(j);
+			    		
+			    		try{
+				    		JsonObject data = (JsonObject) jsonCourse.get("data");
+				    		
+				    		JsonObject distance = (JsonObject) data.get("distance");
+				    		
+				    		Integer distanceValueInt = 0;
+				    		Double distanceValueDbl = 0.0;
+				    		
+				    		try{
+				    			distanceValueDbl = (Double) distance.get("value");
+				    		} catch(ClassCastException e) {
+				    			distanceValueInt = (Integer) distance.get("value");
+				    		}
+				    		
+				    		Integer calories = (Integer) data.get("calories");
+				    		
+				    		Double pace = (Double) data.get("pace");
+				    		
+			    		} catch (JsonException e) {// Exceção lançada normalmente para os casos onde uma das chaves não existe 
+			    			//Do nothing
+			    		}
+			    		
+//			    		JsonObject duration = (JsonObject) data.get("duration");
+//			    		Integer durationValue = (Integer) duration.get("value");
+			    		
+			    	}
+			    	
+			    	String jsonCoursesStr = jsonCourses.toString();
+			    	
+			    	courseLimit += 50;
+			    	coursesStr = "";
+	    		}
+	    	}
+//	    	List<CourseFB>
+	    	
+//	    	JsonObject jsonCourses = facebookClient.fetchObjects(coursesRequest, JsonObject.class);
+	    	
 	    }
+	    
+	    
 	    
 	    
 //	    postFitnessServico.lePostFitnessPorIdPessoa(idPessoa);
--- src/br/com/fitrank/service/CourseServico.java
+++ src/br/com/fitrank/service/CourseServico.java
@@ -1,40 +1,5 @@
 package br.com.fitrank.service;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import br.com.fitrank.modelo.Course;
-import br.com.fitrank.modelo.PostFitness;
-import br.com.fitrank.persistencia.CourseDAO;
-import br.com.fitrank.persistencia.PostFitnessDAO;
-
 public class CourseServico {
-	
-	private CourseDAO courseDAO;
-	
-	public boolean adicionaListaIdsCourseServico(ArrayList<Course> listaCourses) {
 
-		this.courseDAO = new CourseDAO();
-
-		try {
-			return courseDAO.adicionaListaIdsCourse(listaCourses);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public List<Course> leCoursePorIdPessoa(String idPessoa) {
-		this.courseDAO = new CourseDAO();
-		
-		try {
-	    	return courseDAO.leCoursePorIdPessoa(idPessoa);
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-			return null;
-		}
-	}
 }
